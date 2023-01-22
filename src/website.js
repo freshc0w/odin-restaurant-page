3147e22b7449cae291ee9c076a8643a98aa05f16
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-22 05:25:06 +0000

    Add btns click functionality

--- src/website.js
+++ src/website.js
@@ -5,11 +5,56 @@ function createHeader() {
     
     const restaurantName = document.createElement('h1');
     restaurantName.classList.add('restaurantName');
-    restaurantName.textContent = 'Burger Queen';
+    restaurantName.textContent = 'Burger Danger';
 
     header.appendChild(restaurantName);
+    header.appendChild(createNav());
 
     return header;
+}
+
+// Create navabar that includes 'home', 'about', 'menu', 'contact'.
+function createNav() {
+    const nav = document.createElement('nav');
+
+    const createBtn = (text) => {
+        const btn = document.createElement('button');
+        btn.classList.add('btn-nav');
+        btn.textContent = text;
+        return btn;
+        };
+
+    const setActiveBtn = (targetBtn) => {
+        const btns = document.querySelectorAll('.btn-nav');
+        btns.forEach((btn) => {
+            btn.classList.remove('active'); 
+        });
+
+        targetBtn.classList.add('active');
+    }
+
+    const addClickFunction = (targetBtn) => {
+        targetBtn.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;
+        setActiveBtn(targetBtn);
+        })
+    }
+
+    const homeBtn = createBtn('Home');
+    const aboutBtn = createBtn('About');
+    const menuBtn = createBtn('Menu');
+    const contactBtn = createBtn('Contact');
+
+    let btnOptions = [homeBtn, aboutBtn, menuBtn, contactBtn];
+
+    // Add btns into header
+    for(let btn of btnOptions) {
+        addClickFunction(btn);
+        nav.append(btn);
+    }
+
+    return nav;
+
 }
 
 
