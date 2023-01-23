a08a5efac050345f9d7f494ffb960d05c362d778
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-23 04:09:46 +0000

    Add fake address and phone num

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,19 @@
+function createContactPage() {
+    const aboutPage = document.createElement('div');
+    aboutPage.classList.add('about');
+
+    const phoneNum = document.createElement('p');
+    phoneNum.classList.add('contactLine');
+    phoneNum.textContent = '✆ 555-555-5555';
+
+    const address = document.createElement('p');
+    address.classList.add('contactLine');
+    address.textcontent = '🏠 123 Main St, Anytown USA 12345';
+    
+    
+}
+
+
 function loadContact() {
     const main = document.getElementById('main');
     main.textContent = '';
